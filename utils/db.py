6a1683c350e17bde398541a087bcfd14e8d662b9
SergeiKan <kanygin.s.a@gmail.com>
2025-11-23 15:44:27 +0100

    updates

--- utils/db.py
+++ utils/db.py
@@ -10,4 +10,7 @@ DB_NAME = os.getenv("DATABASE_NAME", "styleforecast")
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
+if not MONGO_URI:
+    raise RuntimeError("MONGO_URI is not set. Check your .env file.")
+
 print("✅ Connected to MongoDB:", DB_NAME)
